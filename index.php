f48c0b83e48c5f794b792f1b1f5214aa35920e26
mmugo79 <mugomoses506@gmail.com>
2025-09-02 20:14:18 +0300

    Import instructor files — start of my copy

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+// Include the class file
+require 'ClassAutoLoad.php';
+$layout->header($conf);
+$forms->signup();
+$layout->footer($conf);
---
+++ login.php
@@ -0,0 +1,6 @@
+<?php
+// Include the class file
+require 'ClassAutoLoad.php';
+$layout->header($conf);
+$forms->signin();
+$layout->footer($conf);
